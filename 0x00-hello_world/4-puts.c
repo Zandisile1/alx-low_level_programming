1b8c6a92ba7556ea859e00466fa37705bed18fc7
Zandisile1 <zandisile0701@gmail.com>
2023-06-15 10:07:19 -0700

    Second update of 4-puts.c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
-#include <stdio.h>
-/* Displays a sting output
- * Return 0 if succeful
-*/
-int main(void) {
+#include<stdio.h>
+/**
+ * main - Entry point
+ * *
+ * Return: 0 if succeful
+ * */
+int main(void)
+{
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
