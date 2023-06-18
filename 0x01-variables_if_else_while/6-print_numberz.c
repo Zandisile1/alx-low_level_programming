411f7bd694d2fb96f064441adfa751eb38cd094d
Zandisile1 <zandisile0701@gmail.com>
2023-06-18 04:13:24 -0700

    Created a 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* Main - Entry point
+*
+* Description: 'The program prints all numbers of base 10'
+* Parameter: from 0-10;
+*
+* Return:0 if sucessful
+*/
+
+int main(void) /* my code for numbers of base*/
+{
+int num;
+for (num = 0; num < 10; num++)
+putchar((num % 10) + '0');
+putchar('\n');
+return (0);
+}
