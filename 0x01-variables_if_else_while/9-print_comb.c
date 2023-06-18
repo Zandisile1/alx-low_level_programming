dd1403f8a0c2711a2319249d56ce45c5b333b2dd
Zandisile1 <zandisile0701@gmail.com>
2023-06-18 05:17:25 -0700

    Created a 9-print_com.c file

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * Main -Entry point
+ *
+ * Return:0 if sucessful
+ */
+/* betty style code for function main goes here*/
+int main(void)
+{
+int i = '0';
+while (i <= '9')
+/* this is the code*/
+{
+putchar(i);
+if (i != '9')
+{
+putchar(',');
+putchar(' ');
+}
+i++;
+}
+putchar('\n');
+return (0);
+}
