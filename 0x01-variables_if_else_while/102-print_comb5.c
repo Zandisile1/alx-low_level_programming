2fee23a58dbe181d75c581b64135b1403f0f179e
Zandisile1 <zandisile0701@gmail.com>
2023-06-18 10:53:35 -0700

    Created a 102-print_comb5.c file

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * Main - Prints all the possible combinations.
+ * of two digit numbers without a repeat.
+ *
+ * Return:0 if sucessful.
+ */
+int main(void)
+{
+int i;
+int j;
+for (i = 0; i <= 99; i++)
+{
+for (j = 0; j <= 99; j++)
+{
+if (i < j && i != j)
+{
+putchar((i / 10) + '0');
+putchar((i % 10) + '0');
+putchar(' ');
+putchar((j / 10) + '0');
+putchar((j % 10) + '0');
+if (i != 98 || j != 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
