12cf8af6179326af4fcdda8d9de3628cd52cc90b
Zandisile1 <zandisile0701@gmail.com>
2023-06-20 11:49:57 -0700

    Created a 10-add.c file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers
+ * @d1: first number to be computed
+ * @d2: second number to be computed
+ *
+ * Return: int value.
+ */
+int add(int d1, int d2)
+{
+int results;
+{
+results = d1 + d2;
+}
+{
+return (results);
+}
+}
+
