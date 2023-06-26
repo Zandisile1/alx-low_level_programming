116521a03ef3d9a525e8c91ecbb14369313eeeb7
Zandisile1 <zandisile0701@gmail.com>
2023-06-26 04:43:15 -0700

    Created a 0-reset_to_98.c file

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - a function that takes a pointer to an int
+ * @n: updates the vailue it points to 98
+ *
+ * Return: 0 id sucessful
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
