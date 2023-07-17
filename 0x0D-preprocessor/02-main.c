b73daecf0b67bca6d766f86fd025cc89dc189803
Zandisile1 <zandisile0701@gmail.com>
2023-07-17 02:24:04 -0700

    File name

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * Return: the name of the source file
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
